e93792655ed8b792b9c0cd11cf167e44b36312e7
Dominic Moreno <spartanbloodmoreno@gmail.com>
2025-05-25 09:07:10 -0500

    shopping list

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-// var myArr = [["John", 5], ["Caleb", 7], ["Jimmy", 9]]
+var myList = [["cereal", 1], ["Milk", 2], ["Bananas", 3], ["eggs", 4], ["Sausage", 4]]
 
-// myArr.push(["Johnny", 11]) // Adds items
-
-// console.log(myArr)
-
-// var myArray = [["John", 23], ["cat", 2]]
-
-// var removedFromMyArr = myArray.pop()
-
-// console.log(removedFromMyArr)
-// console.log(myArray)
-
-// var myArray = [["John", 23], ["dog", 3]]
-
-// var removedFromMyArr = myArray.shift()
-
-// console.log(removedFromMyArr)
-// console.log(myArray)
-
-// var myArray = ["Marvel", "StarWars", "Jessie"]
-// myArray.shift()
-// myArray.unshift("Minecraft Movie")
-
-// console.log(myArray)
+console.log(myList)
